5ffe616f2478e2db22d5d97f41c40178c9d43e97
AndrewGavin304 <AndrewGavin304@gmail.com>
2022-09-12 15:04:10 -0400

    begin switch to weather gov

--- src/modules/weather.js
+++ src/modules/weather.js
@@ -4,24 +4,40 @@ function convertCelsiusToFahrenheit(tempCelsius) {
 }
 
 export function processWeatherData(data) {
-  const weatherObject = {
-    tempCelsius: `${data.main.temp}`,
-    tempFahrenheit: convertCelsiusToFahrenheit(`${data.main.temp}`).toString(),
-    tempFeelsLike: `${data.main.feels_like}`,
-    humidity: `${data.main.humidity}`,
-    wind: `${data.wind.speed}`,
-    clouds: `${data.clouds.all}`,
-    description: `${data.weather[0].description}`,
+  const currentWeatherObject = {
+    temp: `${data.properties.periods[0].temperature}`,
+    wind: `${data.properties.periods[0].windSpeed}`,
+    description: `${data.properties.periods[0].shortForecast}`,
+    isDay: `${data.properties.periods[0].isDaytime}`,
   };
-
-  return weatherObject;
+  return currentWeatherObject;
 }
 
-export async function getWeather(location) {
+//TODO: process input to return 3 parameters for more specific locations
+export async function getLatLong(town, state, country) {
+  if (state === undefined) {
+    state = '';
+  }
+  if (country === undefined) {
+    country = '';
+  }
+  const response = await fetch(`http://api.openweathermap.org/geo/1.0/direct?q=${town},${state},${country}&limit=1&appid=8e295eaa7e218e2a1c3e71497e4b9f7b`);
+  return response;
+}
+
+export async function getWeather(town, state, country) {
+  const latLongResponse = await getLatLong(town, state, country);
+  const latLongData = await latLongResponse.json();
+  const lat = `${latLongData[0].lat}`;
+  const lon = `${latLongData[0].lon}`;
+
   const response = await fetch(
-    `https://api.openweathermap.org/data/2.5/weather?q=${location}&APPID=8e295eaa7e218e2a1c3e71497e4b9f7b&units=metric`);
+    `https://api.weather.gov/points/${lat},${lon}`);
   const data = await response.json();
-  console.log(data);
-  const weatherObject = processWeatherData(data);
-  console.log(weatherObject);
+  console.log(data)
+  const forecastAPIURL = data.properties.forecast;
+  const forecastResponse = await fetch(forecastAPIURL);
+  const forecastData = await forecastResponse.json();
+  console.log(forecastData);
+  processWeatherData(forecastData);
 }
